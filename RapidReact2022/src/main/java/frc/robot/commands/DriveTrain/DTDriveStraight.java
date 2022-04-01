e2fd2c37edba6c8ac89ab6b094d47f32a1b376c6
KelvinBM <93287024+KelvinBM@users.noreply.github.com>
2022-04-01 17:49:52 -0400

    changed interrupted method

--- RapidReact2022/src/main/java/frc/robot/commands/DriveTrain/DTDriveStraight.java
+++ RapidReact2022/src/main/java/frc/robot/commands/DriveTrain/DTDriveStraight.java
@@ -51,7 +51,7 @@ public class DTDriveStraight extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    driveTrain._arcadeDrive(0, 0);
+    driveTrain.stopAllMotors();
   }
 
   // Returns true when the command should end.
